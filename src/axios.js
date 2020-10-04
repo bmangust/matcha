0c95d6f99283d4db6338f2178692f07616b6465e
bmangust <ftf.pollux@gmail.com>
2020-10-04 22:35:01 +0300

    Update axios - parametrize host/port

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+const host = "localhost";
+const port = 8080;
+
 export const api = axios.create({
-  baseURL: "http://localhost:8081/api/v1/",
+  baseURL: `http://${host}:${port}/api/v1/`,
   headers: { "Content-Type": "application/json" },
 });
 
 export const media = axios.create({
-  baseURL: "http://localhost:8081/api/media",
+  baseURL: `http://${host}:${port}/api/media`,
   headers: { "Content-Type": "application/json" },
 });
