212ff4b0ba350d14575e960b5ac33bfc836e611e
bmangust <ftf.pollux@gmail.com>
2020-10-26 06:41:19 +0300

    parametrize Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,20 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+// const ip = "192.168.43.151";
+const ip = "localhost";
+const port = 8080;
+
 module.exports = function (app) {
   app.use(
     "/api/main/",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: `http://${ip}:${port}`,
       changeOrigin: true,
     })
   );
   app.use(
     "/api/media/",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: `http://${ip}:${port}`,
       changeOrigin: true,
     })
   );
