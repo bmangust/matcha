60ba6609cc078eb0c7bc72e38605f142bdf4687d
bmangust <ftf.pollux@gmail.com>
2020-11-02 07:02:52 +0300

    Header story

---
+++ src/components/Header/Header.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "./Header";
+export default {
+  component: Header,
+  title: "Header",
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  header: "Chat",
+  notification: new Set([
+    "4567e1e306df5dd2ab333d9d499a1ab2f9071275e21f7d61265af04668b97789",
+    "fe4c83fea2e99350a7d6688de16b2ecdb0074dddea4f86f83ba6b24a1468981a",
+  ]),
+};
