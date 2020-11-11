4be7367d5d216c4799fbb4b796867b6d229561fa
bmangust <ftf.pollux@gmail.com>
2020-11-11 16:30:49 +0300

    update App to show Loader when needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { ThemeProvider } from "@material-ui/core/styles";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./theme";
 
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Main from "./containers/Main/Main";
 import { SnackbarProvider } from "notistack";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AdditionalInfo from "./pages/AdditionalInfo/AdditionalInfo";
+import { getSelfInfo } from "./store/generalSlice";
+import { CircularProgress, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  Grid: {
+    width: "100vw",
+    height: "100vh",
+  },
+});
 
 function App() {
-  const { isAuth } = useSelector((state) => state.general);
+  const classes = useStyles();
+  const { isAuth, isLoading } = useSelector((state) => state.general);
   const { isInfoMissing } = useSelector((state) => state.UI);
+  const dispatch = useDispatch();
+  const [content, setContent] = useState(<CircularProgress />);
 
-  let content = <Login />;
-  // if (isAuth && isInfoMissing) {
-  //   content = (
-  //     <Switch>
-  //       <Redirect from="/login" to="/add" />
-  //       <Route path="/add" component={AdditionalInfo} exact />
-  //     </Switch>
-  //   );
-  // } else
-  if (isAuth) {
-    content = (
-      <Switch>
-        <Redirect exact from="/strangers" to="/" />
-        <Redirect from="/login" to="/" />
-        <Route path="/" component={Main} />
-      </Switch>
-    );
-  } else {
-    content = (
-      <Switch>
-        <Route path="/register" component={Register} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/">
-          <Redirect to="/login" />
-        </Route>
-      </Switch>
-    );
-  }
+  useEffect(() => {
+    dispatch(getSelfInfo());
+  }, []);
+
+  useEffect(() => {
+    if (isLoading) {
+      setContent(
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          className={classes.Grid}
+        >
+          <CircularProgress />
+        </Grid>
+      );
+    } else if (isAuth && isInfoMissing) {
+      setContent(
+        <Switch>
+          <Redirect from="/login" to="/add" />
+          <Route path="/add" component={AdditionalInfo} exact />
+        </Switch>
+      );
+    } else if (isAuth) {
+      setContent(
+        <Switch>
+          <Redirect exact from="/strangers" to="/" />
+          <Redirect from="/login" to="/" />
+          <Route path="/" component={Main} />
+        </Switch>
+      );
+    } else {
+      setContent(
+        <Switch>
+          <Route path="/register" component={Register} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      );
+    }
+  }, [isAuth, isLoading]);
 
   return (
     <ThemeProvider theme={theme}>
